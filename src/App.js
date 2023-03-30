bd34a14fb0511660d6ac9eb8782a8e7bff06a1a9
Daric Teske <dteske25@users.noreply.github.com>
2023-03-29 23:42:07 -0500

    initial layout of buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "react-bootstrap/Button";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div
+      style={{
+        width: "100vw",
+        height: "75vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        alignSelf: "center",
+        gap: "24px",
+      }}
+    >
+      <div
+        style={{
+          flex: "0 0 auto",
+        }}
+      >
+        <img src="https://via.placeholder.com/1000x600" />
+      </div>
+
+      <div>
+        <a href="https://discord.gg/fH6DcwgwVA">
+          <Button variant="outline-primary" size="lg">
+            Join our Discord
+          </Button>
         </a>
-      </header>
+      </div>
+      <div>
+        <a href="https://cerv.is/0119gucg3eW">
+          <Button variant="outline-primary" size="lg">
+            Build Day Sign-ups
+          </Button>
+        </a>
+      </div>
+      <div>
+        <a href="https://habitatomaha.org/donate/young-professionals/">
+          <Button variant="outline-primary" size="lg">
+            Pay Your Dues
+          </Button>
+        </a>
+      </div>
     </div>
   );
 }
